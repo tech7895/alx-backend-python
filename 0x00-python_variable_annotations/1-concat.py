fca4345aad305b9de4e866ebcff73d1cdaad1fcf
tech7895 <coper5540@gmail.com>
2024-06-06 00:39:03 -0700

    Tasks updated

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''This script contains 1's module.
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''Concatenates two strings.
+    '''
+    return str1 + str2
---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''This script contains 4's module.
+'''
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
